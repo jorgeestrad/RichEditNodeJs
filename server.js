567f2f1b13d8dd48fb839c8a0ebb7d8cd0361858
JorgeEstradaRoyal <jorge.estrada@royalsoftware.com.co>
2024-06-04 22:24:02 -0500

    avances nocturnos

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended : true}));
 
 const db = admin.firestore();
 
-app.post('/create',async (req,res) => {
+app.post('/api/createTemplate',async (req,res) => {
     try {
         
         console.log(req.body);
@@ -34,7 +34,7 @@ app.post('/create',async (req,res) => {
 
         console.log(documentoJson);
         console.log(id);
-        const response = db.collection("expedientes").doc(id).set(documentoJson);
+        const response = db.collection("templates").doc(id).set(documentoJson);
         res.send(response);
         console.log(response);
     } catch(error){
@@ -43,7 +43,76 @@ app.post('/create',async (req,res) => {
     }
 })
 
+// read item
+app.get('/api/readTemplate/:id', (req, res) => {
+    (async () => {
+        try {
+            const document = db.collection('templates').doc(req.params.id);
+            let item = await document.get();
+            let response = item.data();
+            return res.status(200).send(response);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+        })();
+    });
 
+
+    // read all
+app.get('/api/readAllTemplates', (req, res) => {
+    (async () => {
+        try {
+            let query = db.collection('templates');
+            let response = [];
+            await query.get().then(querySnapshot => {
+            let docs = querySnapshot.docs;
+            for (let doc of docs) {
+                const selectedItem = {
+                    id: doc.id,
+                    item: doc.data().item
+                };
+                response.push(selectedItem);
+            }
+            });
+            return res.status(200).send(response);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+        })();
+    });
+    
+    // update
+app.put('/api/updateTemplate/:id', (req, res) => {
+    (async () => {
+        try {
+            const document = db.collection('templates').doc(req.params.id);
+            await document.update({
+                item: req.body.item
+            });
+            return res.status(200).send();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+        })();
+    });
+
+
+    // delete
+app.delete('/api/deleteTemplate/:id', (req, res) => {
+    (async () => {
+        try {
+            const document = db.collection('templates').doc(req.params.id);
+            await document.delete();
+            return res.status(200).send();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+        })();
+    });
 
 const PORT =  process.env.PORT || 8080;
 app.listen(PORT, () => {
